fbd1f2612e84e23a393ffec54575e2002bfafb6b
Jun <jzheng>
2026-04-24 16:11:20 +0200

    se añade un trozo de codigo en el next.config.mjs que es el encargado de la declaracion de como se tiene que realizar el npm run build lo que se ha añadido sirve para que ignore los errores de tipo TS en el build, es decir, si tenemos un archivo tipo page.jsx que se le pasa valores personalizados que ignore ese tipo de error ya que el framework solo acepta que los page.jsx se pasen valores predefinidos y no personalizados y al hacer el buidl da error pero no al hacer npm run dev

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@
 const nextConfig = {
   output: 'export', //-> Crea una compilación estática para subirlos al servidor, crea la carpeta compilada /out
   trailingSlash: true, //-> Sirve para que al actualizar una pagina se mantenga en la misma pagina y no regrese al index.html
+  typescript: {
+    ignoreBuildErrors: true, //-> Ignora errores de tipos TS en el build (pages con props personalizadas)
+  },
 }
  
 export default nextConfig
